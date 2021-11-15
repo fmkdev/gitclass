ec55cd717d58a2f6bbf9eace67c5610f74630b8e
fmkdev <folarinmuhammedfathi.k@gmail.com>
2021-11-15 11:55:38 +0100

    created a new csharp project and added a line of code

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("FMK Dev!");
+        }
+    }
+}
